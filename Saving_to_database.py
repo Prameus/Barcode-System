b8437c7ab1147494060bcd3b5303d3535c8689bb
musa <musa_karaaslan2@hotmail.com>
2022-01-25 16:49:59 +0300

    commit

--- Saving_to_database.py
+++ Saving_to_database.py
@@ -23,46 +23,49 @@ try:
     cursor = connect.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
     class Database:
+        products = []
+
         def __init__(self):
             #cursor.execute('DROP TABLE IF EXISTS products')
 
             create_script = """
                     CREATE TABLE IF NOT EXISTS products (
                         id          INT PRIMARY KEY,
-                        product     varchar(100),
+                        product     TINYTEXT,
                         carat       int,
                         barcode     int
                     )
                     """
             cursor.execute(create_script)
 
-        def adding_product(self):
-            values_scripts = []
-            for i in fields:
-                value = str(input(f'Eklemek istediginiz urunun {i} yaziniz: '))
-                values_scripts.append(value)
+        def adding_product(self, *kwargs):
             insert_new_product = 'INSERT INTO products (id,product,carat,barcode) VALUES (%s,%s,%s,%s) '
-            print(values_scripts)
-            cursor.execute(insert_new_product, values_scripts)
+            cursor.execute(insert_new_product, kwargs)
 
         def read_all_products(self):
-            products = []
+
             cursor.execute('SELECT * FROM PRODUCTS')
             for record in cursor.fetchall():
-               products.append(record)
-            print(products)
-            return products
-            
-        def delete_product(self):
-            value = input('Silmek istediginiz urunun isimini giriniz: ',)
-            modified_value = str(f'{value}',)
-            delete_product = "DELETE FROM products WHERE id = %s"
-            print(modified_value)
-            cursor.execute(delete_product, modified_value)
+                self.products.append(record)
+            return self.products
+
+        def clear_all(self):
+            self.products.clear()
+
+        def delete_product(self, selected):
+            int_selected = int(selected)
+            delete_product = (
+                "DELETE FROM products WHERE id = %s")
+            cursor.execute(delete_product, int_selected)
+            # NEDEN SADECE 1 ITEM SILINIYOR??????
+
+        def update_product(self, *kwargs):
+            update_command = "UPDATE products SET (%s) = (%s) WHERE id = (%s);"
+            cursor.execute(update_command, kwargs)
+            # update syntax`inda hata yapiyorum ama nasil?
 
     database = Database()
     database.read_all_products()
-
     connect.commit()
 except Exception as error:
     print(error)
--- new_gui_screen.py
+++ new_gui_screen.py
@@ -1,8 +1,10 @@
+from itertools import product
 import tkinter as tk
 from tkinter import Entry, Label, Toplevel, ttk
 import os
 from tkinter import messagebox
 from tkinter import font
+from turtle import width
 
 from matplotlib.pyplot import text
 import Saving_to_database
@@ -15,7 +17,7 @@ class Gui:
     count = 0
 
     def get_all_informations(self):
-        label = tk.Label(frame, bg='gray', text='aaaaa')
+        label = tk.Label(self.frame, bg='gray', text='aaaaa')
         label.pack()
 
     def read_all(self):
@@ -24,16 +26,20 @@ class Gui:
             my_tree.insert(parent='', index='end', iid=self.count,
                            values=(record[0], record[1], record[2], record[3]))
             self.count = self.count + 1
-        data.clear()
+
 # database`den cektigim bilgileri durmadan geliyor ve 1 2 , 1 2 olarak durmadan dusuyor?
 
-    def selected_item(self, event):
-        global pop, barcode_entry, product_entry, carat_entry, values, selected
+    def delete_row(self):
+        selected = my_tree.focus()
+        Saving_to_database.database.delete_product(selected)
 
+    def add_product(self):
         selected = my_tree.focus()
         values = my_tree.item(selected, 'values')
-        print(selected)
-        print(values)
+        self.pop_screen(values)
+
+    def pop_screen(self, values):
+        global product_entry, carat_entry, barcode_entry, pop
 
         pop = Toplevel(root)
         pop.title('Uyari')
@@ -54,22 +60,30 @@ class Gui:
         barcode_entry = Entry(pop, width=40)
         barcode_entry.grid(column=1, row=2)
 
-        product_entry.insert(0, values[1])
-        carat_entry.insert(0, values[2])
-        barcode_entry.insert(0, values[3])
-
         approve = tk.Button(pop, text='Approve', width=5,
-                            padx=10, pady=5, fg='white', bg="#263d42", command=self.approve_values)
+                            padx=10, pady=5, fg='white', bg="#263d42", command=self.approve_values_add)
         approve.grid(column=0, row=5, padx=40)
+
         cancel = tk.Button(pop, text='Cancel', width=5,
                            padx=10, pady=5, fg='white', bg="#263d42", command=self.cancel_process)
         cancel.grid(column=1, row=5)
 
-    def approve_values(self):
+    def selected_item(self, event):
+        global selected, values
+
+        selected = my_tree.focus()
+        values = my_tree.item(selected, 'values')
+        self.pop_screen()
+
+        product_entry.insert(0, values[1])
+        carat_entry.insert(0, values[2])
+        barcode_entry.insert(0, values[3])
+
+    def approve_values_update(self):
         product_entry_info = product_entry.get()
         carat_entry_info = int(carat_entry.get())
         barcode_entry_info = int(barcode_entry.get())
-        selected = + 1
+
         Saving_to_database.database.update_product(
             'product', product_entry_info, selected)
         Saving_to_database.database.update_product(
@@ -77,47 +91,21 @@ class Gui:
         Saving_to_database.database.update_product(
             'barcode', barcode_entry_info, selected)
 
-    def delete_row(self):
-        selected = my_tree.focus()
-        print(type(selected))
-        Saving_to_database.database.delete_product(selected)
+    def approve_values_add(self):
+        product_entry_info = product_entry.get()
+        carat_entry_info = int(carat_entry.get())
+        barcode_entry_info = int(barcode_entry.get())
 
-    def approve_new_item(self):
-        last_id = self.count + 1
         Saving_to_database.database.adding_product(
-            last_id, product_entry, carat_entry, barcode_entry)
-
-    def add_product(self):
-
-        pop = Toplevel(root)
-        pop.title('Uyari')
-        pop.geometry('350x230')
-
-        product_label = Label(pop, text='Product:')
-        product_label.grid(column=0, row=0, ipady=20,)
-        product_entry = Entry(pop)
-        product_entry.grid(column=1, row=0, sticky=tk.W)
-
-        carat_label = Label(pop, text='Carat:')
-        carat_label.grid(column=0, row=1, ipady=20, sticky=tk.W)
-        carat_entry = Entry(pop, width=15)
-        carat_entry.grid(column=1, row=1, sticky=tk.W)
-
-        barcode_label = Label(pop, text='Barcode:')
-        barcode_label.grid(column=0, row=2, ipady=20, sticky=tk.W)
-        barcode_entry = Entry(pop, width=40)
-        barcode_entry.grid(column=1, row=2)
-
-        approve = tk.Button(pop, text='Approve', width=5,
-                            padx=10, pady=5, fg='white', bg="#263d42", command=self.approve_new_item)
-        approve.grid(column=0, row=5, padx=40)
-
-        cancel = tk.Button(pop, text='Cancel', width=5,
-                           padx=10, pady=5, fg='white', bg="#263d42", command=self.cancel_process)
-        cancel.grid(column=1, row=5)
+            (self.count + 1), product_entry_info, carat_entry_info, barcode_entry_info)
 
     def cancel_process(self):
         pop.destroy()
+
+    def clear_all(self):
+        for i in my_tree.get_children():
+            my_tree.delete(i)
+        Saving_to_database.database.clear_all()
 
 
 gui = Gui()
@@ -143,8 +131,8 @@ frame2 = tk.Frame(root, bg="#fff")
 frame2.place(relheight=0.8, relwidth=0.2, relx=0.78, rely=0.1)
 
 my_tree = ttk.Treeview(frame, height=15)
-my_tree['columns'] = ('ID', 'product', 'carat', 'barcode')
 
+my_tree['columns'] = ('ID', 'product', 'carat', 'barcode')
 
 my_tree.column('#0', width=0, stretch=0)
 my_tree.column('ID', width=180, anchor=tk.CENTER)
@@ -177,5 +165,8 @@ adding_button = tk.Button(frame2, text='Add Product', width=30,
                           padx=10, pady=5, fg='white', bg="#263d42", command=gui.add_product)
 adding_button.pack()
 
+clear_all_button = tk.Button(frame2, text='Clear All', width=30,
+                             padx=10, pady=5, fg='white', bg="#263d42", command=gui.clear_all)
+clear_all_button.pack()
 
 root.mainloop()
